52bd8911145f99c44fdeafe0bfbd8526d71b61bd
shobu008 <shobu.okamoto@gmail.com>
2023-03-09 22:48:52 +0900

    課題の提出

---
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -0,0 +1,54 @@
+package com.techacademy;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KadaiFirstController {
+
+    @GetMapping("dayofweek/20230308")
+    public String dispDayOfWeek() {
+
+        LocalDate date = LocalDate.of(2023,3,8);
+
+        DayOfWeek w = date.getDayOfWeek();
+
+        System.out.println(w);
+
+        return "実行結果：" + w;
+    }
+
+    @GetMapping("/plus/{val1}/{val2}")
+    public String calcPlus(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 + val2;
+        return "実行結果：" + res;
+    }
+
+    @GetMapping("/minus/{val1}/{val2}")
+    public String calcMinus(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 - val2;
+        return "実行結果：" + res;
+    }
+
+    @GetMapping("/times/{val1}/{val2}")
+    public String calcTimes(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 * val2;
+        return "実行結果：" + res;
+    }
+
+    @GetMapping("/divide/{val1}/{val2}")
+    public String calcDivide(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 / val2;
+        return "実行結果：" + res;
+    }
+
+
+}
